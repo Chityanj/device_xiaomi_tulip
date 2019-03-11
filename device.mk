skinzor/android_device_xiaomi_whyred-1

whyred: Build Trust HAL
Change-Id: I92706987f8d3094cdfb82e71e5331b7efe69e5df
Signed-off-by: Joey <joey@lineageos.org>

device.mk
@@ -419,6 +419,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libtinyxml2
 
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.mokee.trust@1.0-service
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic

Chityanj/device_xiaomi_tulip

device.mk
@@ -403,6 +403,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libtinyxml2
 
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic